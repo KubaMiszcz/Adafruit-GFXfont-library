fdc45672f865aebaf3f9127b83fb8e3ddea4984e
KubaMiszcz <zielonyeufor@gmail.com>
2025-05-09 10:06:26 +0200

    update script

--- scripts/convertbact.py
+++ scripts/convertbact.py
@@ -10,7 +10,7 @@ fontSizes = [6, 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28]
 fontList = os.listdir()
 cwd = os.getcwd()
 fontList = os.listdir(cwd+rf'\fontsToImport')
-
+GFX_FontsDirName = 'GFX_Fonts'
 
 def isFont(font):
     ext = Path(font).suffix.lower()
@@ -25,32 +25,38 @@ def line_prepender(filename, line):
         f.seek(0, 0)
         f.write(line.rstrip('\r\n') + '\n' + content)
 
+# // START
+# // START
+# // START
 print('start')
+# input("Press Enter to continue...")
 
 # // create directories and copy font into them
 file1 = open("addedFonts.txt", "w")
+dirNamesList = []
 for font in fontList:
     if isFont(font):
         dirName = font
         dirName = dirName.replace('.', '_')
         dirName = dirName.replace(' ', '_')
-        dirPath = cwd + rf'\GFX_Fonts' + '\\' + dirName
+        dirPath = cwd + rf'\\'+ GFX_FontsDirName + '\\' + dirName
         if not Path(dirPath).exists():
             os.mkdir(dirPath)
+        dirNamesList.append(dirName)
 
         fontName = font
         src_dir = cwd + rf'\fontsToImport' + '\\' + font
         dst_dir = dirPath + '\\' + font
         shutil.copy(src_dir, dst_dir)
 
-dirList = os.listdir(cwd+rf'\GFX_Fonts')
-for dirName in dirList:
-    fontList = os.listdir(cwd+rf'\GFX_Fonts'+ rf'\\' + dirName)
-        
+# dirList = os.listdir(cwd+rf'\GFX_Fonts')
+for dirName in dirNamesList:
+    fontList = os.listdir(cwd+rf'\\' + GFX_FontsDirName + rf'\\' + dirName)
+
     for font in fontList:
         if isFont(font):
             # convert fonts
-            dirPath = cwd + rf'\GFX_Fonts' + '\\' + dirName
+            dirPath = cwd + rf'\\' + GFX_FontsDirName + '\\' + dirName
             srcFont = font
             gfxFontName = Path(srcFont).stem
 
@@ -60,7 +66,7 @@ for dirName in dirList:
                 # `fontconvert myCoolFont.ttf 9 > myCoolFont9pt.h`
                 pathToBinary = rf'{cwd}/GFX_Font_Converter-binary/fontconvert.exe'
                 srcFile = f'"{dirPath}/{font}"'
-                outputfile = rf'{dirPath}/{gfxFontName}{size}pt.h'
+                outputfile = rf'{dirPath}/{gfxFontName}_{size}pt.h'
                 cmd = rf'{pathToBinary} {srcFile} {size} > {outputfile}'
                 os.system(cmd)
 
@@ -68,10 +74,12 @@ for dirName in dirList:
                 line_prepender(outputfile,'#include <Adafruit_GFX.h>')
                 line_prepender(outputfile,'#pragma once')
 
-                print(f'converted {font} into {gfxFontName}{size}pt.h')
+                print(f'converted {font} into {gfxFontName}_{size}pt.h')
 
             # add to list
             file1.write(f"- {font}\n")
 
 print("finished")
 file1.close()
+input("Press Enter to continue...")
+# // END
